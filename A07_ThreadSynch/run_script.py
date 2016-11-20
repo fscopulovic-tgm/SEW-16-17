4ecd21e89339fbdf1144d8aeb1f72deff4e05713
fscopulovic-tgm <fscopulovic@student.tgm.ac.at>
2016-11-20 21:35:27 +0100

    Modified A07_ThreadSynch

--- A07_ThreadSynch/run_script.py
+++ A07_ThreadSynch/run_script.py
@@ -3,22 +3,39 @@
 @date: 11-11-2016
 @use: run_script that initializes producer and consumer and starts them
 """
-import queue, consumer, producer, os
-"""
-Starts the threads and if the user gives in a input it reacts to that.
-If the user types in exit, it breaks the endless loop and calls the method .join(), so it terminates all the threads
+import queue, consumer, producer
 
-:return None:
-"""
-queue = queue.Queue()
+class Run_script:
+    """
+    Class I made for starting the Consumer and producer
+    I just made the class so my docstrings will be recognized by Sphinx
+    """
 
-file = open("files/prime_numbers.txt", 'w')
+    def __init__(self):
+        """
+        Initializes the queue, the file, the Consumer and the Producer and calls the start_threads() method
+        """
+        self.queue = queue.Queue()
 
-t_producer = producer.Producer(queue)
-t_consumer = consumer.Consumer(queue, file)
+        self.file = open("files/prime_numbers.txt", 'w')
 
-t_producer.start()
-t_consumer.start()
+        self.t_producer = producer.Producer(self.queue)
+        self.t_consumer = consumer.Consumer(self.queue, self.file)
 
-t_producer.join()
-t_consumer.join()
+        self.start_threads()
+
+
+    def start_threads(self):
+        """
+        Calls the methods start() and join() from Producer and Consumer
+
+        :return None:
+        """
+        self.t_producer.start()
+        self.t_consumer.start()
+
+        self.t_producer.join()
+        self.t_consumer.join()
+
+
+rs = Run_script()
